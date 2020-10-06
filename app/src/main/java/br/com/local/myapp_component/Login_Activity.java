a08d503bf08f1d98c116bae1aa532718251f339f
Backds55 <luihiroshi6@gmail.com>
2020-10-05 22:23:26 -0300

    Aula 05/10 - itens de menu, login com autenticação com splash - PAMI

--- app/src/main/java/br/com/local/myapp_component/Login_Activity.java
+++ app/src/main/java/br/com/local/myapp_component/Login_Activity.java
@@ -3,12 +3,44 @@ package br.com.local.myapp_component;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login_Activity extends AppCompatActivity {
+
+    Button btnEntrar, btnSair;
+    EditText txtEmail, txtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnEntrar = findViewById(R.id.btEntrar);
+        btnSair = findViewById(R.id.btSair);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha)
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Transformando as variáveis edittext em string
+                String email = txtEmail.getText().toString();
+                String senha = txtSenha.getText().toString();
+                if(email.equals("etecia")&& senha.equals("etecia")) {
+                    startActivity(new Intent(getApplicationContext(),
+                            MenuPrincipal_Activity.class));
+                    finish();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),
+                            "Usuário e senha incorretos",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/br/com/local/myapp_component/Vingadores_Activity.java
+++ app/src/main/java/br/com/local/myapp_component/Vingadores_Activity.java
@@ -1,8 +1,14 @@
 package br.com.local.myapp_component;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Vingadores_Activity extends AppCompatActivity {
 
@@ -10,5 +16,56 @@ public class Vingadores_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vingadores_layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_vingadores, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mCAmerica:
+                startActivity(new Intent(getApplicationContext(),
+                        CAmerica_Activity.class));
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no Capitão América",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mHFerro:
+                startActivity(new Intent(getApplicationContext(),
+                        HFerro_Activity.class));
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no Homem de Ferro",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mHulk:
+                startActivity(new Intent(getApplicationContext(),
+                        Hulk_Activity.class));
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no Hulk",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mGArqueiro:
+                startActivity(new Intent(getApplicationContext(),
+                        GArqueiro_Activity.class));
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no Gavião Arqueiro",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mVNegra:
+                startActivity(new Intent(getApplicationContext(),
+                        VNegra_Activity.class));
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei na Viúva Negra",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
